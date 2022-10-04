d50b7b6d1df4fc1d3bcb8fa803ec518ffab14fde
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-10-05 00:10:17 +0100

    created dynamic form

--- components/contact/Contact.jsx
+++ components/contact/Contact.jsx
@@ -1,12 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { ContactStyle } from "./styled/ContactStyled";
 
 function Contact() {
   const phoneRegExp = /^[0-9]\d{10}$/;
+  const renEnergy = ["Solar Panels", "Inverters", "Batteries", "UPS"];
+  const security = [
+    "CCTV",
+    "Burglary Alarm",
+    "Electric Fence",
+    "Fire Alarm",
+    "Biometric Door Lock",
+  ];
+  const data = ["WLAN", "VSAT", "FTH", "Networking"];
+  const homeAuto = [
+    "Sound Systems",
+    "Video & visuals",
+    "IP telephoning",
+    "VOIP",
+    "Smart homes",
+    "Intercom",
+  ];
 
   const [service, setService] = useState("");
+  const [type, setType] = useState([]);
+
+  const handleServiceSelect = (e) => {
+    setService(e.target.value);
+  };
+
+  useEffect(() => {
+    service === "renewable energy"
+      ? setType(renEnergy)
+      : service === "security"
+      ? setType(security)
+      : service === "data"
+      ? setType(data)
+      : service === "home automation"
+      ? setType(homeAuto)
+      : setType([]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [service]);
 
   return (
     <ContactStyle>
@@ -20,7 +55,13 @@ function Contact() {
         </p>
       </div>
       <Formik
-        initialValues={{ firstName: "", email: "", service: "", phone: "" }}
+        initialValues={{
+          firstName: "",
+          email: "",
+          service: service,
+          phone: "",
+          serviceType: "",
+        }}
         validationSchema={Yup.object({
           firstName: Yup.string()
             .max(15, "Must be 15 characters or less")
@@ -34,7 +75,7 @@ function Contact() {
             .max(11, "to long"),
         })}
         onSubmit={(values) => {
-          fetch("https://formsubmit.co/ajax/dellmarvissystem@gmail.com", {
+          fetch("https://formsubmit.co/ajax/tomiaiyeniko@gmail.com", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -43,8 +84,9 @@ function Contact() {
             body: JSON.stringify({
               name: values.firstName,
               email: values.email,
-              service: values.service,
+              service: service,
               phoneNo: values.phone,
+              serviceType: values.serviceType,
             }),
           })
             .then((response) => response.json())
@@ -65,7 +107,13 @@ function Contact() {
           <Field name="phone" type="tel" placeholder="Phone" />
           <ErrorMessage name="phone" />
 
-          <Field name="service" as="select" className="my-select">
+          <Field
+            name="service"
+            as="select"
+            className="my-select"
+            value={service}
+            onChange={(e) => handleServiceSelect(e)}
+          >
             <option value="select" className="default">
               Select service
             </option>
@@ -74,6 +122,17 @@ function Contact() {
             <option value="home automation">Home Automation</option>
             <option value="data">Data</option>
           </Field>
+
+          {service.length > 0 && (
+            <Field name="serviceType" as="select" className="my-select">
+              <option value="">Specific service</option>
+              {type.map((ty) => (
+                <option value={ty} key={ty}>
+                  {ty}
+                </option>
+              ))}
+            </Field>
+          )}
 
           <button type="submit">
             <span>Submit</span>
