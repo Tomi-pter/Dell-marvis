26e66488f245975e269a769ffa1d5ddc244d91dc
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-10-05 23:41:11 +0100

    added socials

--- components/index/Footer.jsx
+++ components/index/Footer.jsx
@@ -1,4 +1,11 @@
+import Link from "next/link";
 import { FooterStyle } from "./styled/FooterStyled";
+import {
+  Instagram,
+  LinkedinSquare,
+  Twitter,
+  FacebookSquare,
+} from "styled-icons/boxicons-logos";
 
 function Footer({ children }) {
   return (
@@ -7,15 +14,71 @@ function Footer({ children }) {
       <FooterStyle>
         <div className="container">
           <nav>
-            <div className="logo"></div>
+            <div className="logo">logo</div>
             <ul>
-              <li>Home</li>
-              <li>Solar Panels</li>
-              <li>Inverters</li>
-              <li>Batteries</li>
+              <li>
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/renewable">
+                  <a>Renewable Energy</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/security">
+                  <a>Security</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/homeAuto">
+                  <a>Home Automation</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/data">
+                  <a>Data</a>
+                </Link>
+              </li>
             </ul>
           </nav>
-          <div className="text">text</div>
+          <div className="text">
+            <div className="location">
+              <h2>Central Office</h2>
+              <p>Block A, 2nd Floor, Bright Star Plaza</p>
+              <p>No. 50, Ebitu Ukiwe Street, Jabi, Abuja</p>
+            </div>
+            <div className="phone">
+              <h2>Contact Us (Central Office)</h2>
+              <p>P: 08175314151 | 07030975327</p>
+              <a href="mailto:dellmarvissystem@gmail.com">
+                M: dellmarvissystem@gmail.com
+              </a>
+            </div>
+            <div className="socials">
+              <Link href="https://facebook.com">
+                <a>
+                  <FacebookSquare />
+                </a>
+              </Link>
+              <Link href="https://instagram.com">
+                <a>
+                  <Instagram />
+                </a>
+              </Link>
+              <Link href="https://linkedin.com">
+                <a>
+                  <LinkedinSquare />
+                </a>
+              </Link>
+              <Link href="https://twitter.com">
+                <a>
+                  <Twitter />
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
       </FooterStyle>
     </>
--- components/index/styled/FooterStyled.jsx
+++ components/index/styled/FooterStyled.jsx
@@ -42,6 +42,11 @@ export const FooterStyle = styled.footer`
     letter-spacing: 0.125rem;
     color: white;
     text-transform: uppercase;
+    transition: color 350ms;
+
+    &:hover {
+      color: var(--text-orange);
+    }
   }
 
   li:not(:first-child) {
@@ -49,14 +54,45 @@ export const FooterStyle = styled.footer`
   }
 
   .text {
+    color: white;
     display: grid;
     grid-gap: 3rem;
     margin-top: 3rem;
   }
 
+  .location,
+  .phone {
+    h2,
+    p,
+    a {
+      opacity: 0.5;
+    }
+  }
+
+  .phone a:hover {
+    opacity: 1;
+  }
+
+  .socials {
+    display: flex;
+    justify-content: center;
+
+    svg {
+      width: 30px;
+      height: 30px;
+      fill: white;
+      margin: 0 0.5rem 0.5rem;
+      transition: fill 350ms;
+
+      &:hover {
+        fill: var(--text-orange);
+      }
+    }
+  }
+
   @media screen and (min-width: 641px) {
     .container {
-      padding: 3.75rem 1.5rem 2rem;
+      padding: 3.75rem 1.5rem;
     }
     .container::before {
       left: 1.5rem;
@@ -64,16 +100,46 @@ export const FooterStyle = styled.footer`
     }
     nav {
       flex-direction: row;
+      justify-content: space-between;
+    }
+    .logo {
+      color: white;
     }
     ul {
       margin: 0;
       flex-direction: row;
+      flex-wrap: wrap;
     }
     li:not(:first-child) {
-      margin: 0 0 0 2rem;
+      margin: 0 0 0 1.25rem;
+    }
+    .text {
+      grid-template-columns: 1fr 1fr 1fr;
+      text-align: left;
+
+      h2 {
+        font-size: 0.9rem;
+      }
+      p,
+      a {
+        font-size: 0.8rem;
+      }
+    }
+    .socials {
+      align-items: center;
     }
   }
 
-  @media screen and (min-width: 1008px) {
+  @media screen and (min-width: 100px) {
+    .socials {
+      align-items: flex-end;
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    .container {
+      max-width: 1440px;
+      margin: 0 auto;
+    }
   }
 `;
