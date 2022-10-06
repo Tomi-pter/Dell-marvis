475b27b185a2c4b56d6718fa1fa1d4f94c93be9a
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-10-06 22:04:38 +0100

    scrollTrigger animations

--- components/index/ProDetail.jsx
+++ components/index/ProDetail.jsx
@@ -4,8 +4,85 @@ import { ProDetailStyle } from "./styled/ProDetailStyled";
 import tenKVA from "../../public/saved/10kva-transparent.png";
 import smart from "../../public/saved/smart-transparent.png";
 import router from "../../public/saved/router.png";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 function ProDetail() {
+  useEffect(() => {
+    gsap.defaults({
+      duration: 1,
+    });
+
+    gsap.from(".detail .img", {
+      x: "-500%",
+      scrollTrigger: {
+        trigger: ".detail",
+        markers: true,
+        start: "top 50%",
+      },
+    });
+    gsap.from(".detail .text *", {
+      x: "500%",
+      delay: 0.5,
+      stagger: 0.5,
+      scrub: true,
+      scrollTrigger: {
+        trigger: ".detail",
+        markers: true,
+        start: "top 50%",
+      },
+    });
+
+    gsap.from(".basic .img", {
+      x: "500%",
+      scrollTrigger: {
+        trigger: ".basic",
+        start: "top 50%",
+      },
+    });
+    gsap.from(".basic .text *", {
+      x: "-500%",
+      delay: 0.5,
+      stagger: 0.5,
+      scrub: true,
+      scrollTrigger: {
+        trigger: ".basic",
+        start: "top 50%",
+      },
+    });
+
+    gsap.from(".grid .img", {
+      x: "-500%",
+      scrollTrigger: {
+        trigger: ".grid",
+        start: "top 50%",
+      },
+    });
+    gsap.from(".grid .text *", {
+      x: "500%",
+      delay: 0.5,
+      stagger: 0.5,
+      scrub: true,
+      scrollTrigger: {
+        trigger: ".grid",
+        start: "top 50%",
+      },
+    });
+
+    gsap.from(".dell *", {
+      y: "500%",
+      stagger: 0.5,
+      scrub: true,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".dell",
+        start: "top 50%",
+      },
+    });
+  }, []);
+
   return (
     <ProDetailStyle>
       <article>
@@ -55,7 +132,7 @@ function ProDetail() {
           </div>
         </div>
 
-        <aside>
+        <aside className="dell">
           <h2>
             Bringing <span>Comfort</span> to your doorsteps
           </h2>
--- components/index/Quote.jsx
+++ components/index/Quote.jsx
@@ -1,14 +1,46 @@
 import Link from "next/link";
 import { QuoteStyle } from "./styled/QuoteStyled";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useRef, useEffect } from "react";
+gsap.registerPlugin(ScrollTrigger);
 
 function Quote() {
+  const footRef = useRef();
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+    gsap.from(".contactAside a", {
+      scrollTrigger: {
+        trigger: ".contactAside",
+        start: "top 70%",
+        // end: "bottom",
+        // markers: true,
+        // toggleActions: "restart complete reverse reverse",
+      },
+      y: "200%",
+      delay: 0.5,
+    });
+
+    gsap.from(".contactAside .text", {
+      scrollTrigger: {
+        trigger: ".contactAside",
+        start: "top 70%",
+        // toggleActions: "restart complete reverse reverse",
+      },
+      y: "300%",
+    });
+  }, []);
+
   return (
-    <QuoteStyle>
-      <h2>Let&apos;s talk about your needs!</h2>
-      <p>
-        Interested? Contact us today to discuss specifications for your home,
-        office or building.
-      </p>
+    <QuoteStyle className="contactAside">
+      <div className="text">
+        <h2>Let&apos;s talk about your needs!</h2>
+        <p>
+          Interested? Contact us today to discuss specifications for your home,
+          office or building.
+        </p>
+      </div>
       <Link href="/contact">
         <a>
           <span>Get Quote</span>
